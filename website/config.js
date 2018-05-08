f4297c1697576ebc243f468bc37d9a25d487b8e7
donjor <donjordev@gmail.com>
2018-05-09 01:04:18 +1200

    init commit

--- website/config.js
+++ website/config.js
@@ -1,17 +1,17 @@
-var api = "https://pool.sumokoin.ch:8888";
+var api = "https://107.swag.80.92:8888";
 
-var poolHost = "pool.sumokoin.ch";
+var poolHost = "pool.havenprotocol.com";
 
-var irc = "irc.freenode.net/#sumokoin";
+var irc = "irc.freenode.net/#haven";
 
-var email = "support@sumokoin.ch";
+var email = "havencurrency@gmail.com";
 
 var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://explorer.sumokoin.com/block/{id}";
+var blockchainExplorer = "http://explorer.havenprotocol.com/block/{id}";
 
-var transactionExplorer = "http://explorer.sumokoin.com/tx/{id}";
+var transactionExplorer = "http://explorer.havenprotocol.com/tx/{id}";
 
 var themeCss = "themes/default-theme.css";
